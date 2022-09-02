50f9ea3da2ea9ab766a9df09def0586f4d6cd31c
lucasdias4 <diasc.lucas@gmail.com>
2022-09-02 19:55:33 +0100

    feature: buildSrc module created

---
+++ buildSrc/src/main/java/com/lucasdias/buildSrc/Dependency.kt
@@ -0,0 +1,164 @@
+package com.lucasdias.buildSrc
+
+/**
+ * Article with similar implementation: https://proandroiddev.com/gradle-dependency-management-with-kotlin-94eed4df9a28
+ */
+object Dependency {
+
+    object Core {
+        const val appcompat: String = "androidx.appcompat:appcompat:" + Versions.app_compat
+
+        const val core_ktx: String = "androidx.core:core-ktx:" + Versions.core_ktx
+
+        const val coroutines_android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:" + Versions.coroutines
+
+        const val coroutines_core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.coroutines
+
+        const val lifecycle_view_model = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
+
+        const val legacy_support: String =
+            "androidx.legacy:legacy-support-v4:" + Versions.legacy_support
+
+        const val navigation_fragment: String = "androidx.navigation:navigation-fragment-ktx:" + Versions.navigation
+
+        const val navigation_ui: String = "androidx.navigation:navigation-ui-ktx:" + Versions.navigation
+
+        const val kotlin_stdlib: String = "org.jetbrains.kotlin:kotlin-stdlib:" + Versions.kotlin
+
+        const val kotlin_stdlib_jdk7: String =
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk7:" + Versions.kotlin
+
+        const val kotlin_stdlib_jdk8: String =
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:" + Versions.kotlin
+
+        const val timber = "com.jakewharton.timber:timber:" + Versions.timber
+    }
+
+    object Data {
+
+        const val converter_gson: String =
+            "com.squareup.retrofit2:converter-gson:" + Versions.retrofit
+
+        const val preference: String =
+            "androidx.preference:preference-ktx:" + Versions.preference
+
+        const val logging_interceptor: String = "com.squareup.okhttp3:logging-interceptor:" + Versions.logging_interceptor
+
+        /**
+         * https://github.com/square/retrofit/
+         */
+        const val retrofit: String = "com.squareup.retrofit2:retrofit:" + Versions.retrofit
+    }
+
+    object DI {
+        /**
+         * https://github.com/InsertKoinIO/koin
+         */
+        const val koin: String = "io.insert-koin:koin-android:" + Versions.koin
+    }
+
+    object External {
+
+        const val firebase_analytics: String = "com.google.firebase:firebase-analytics-ktx"
+
+        const val firebase_bom: String = "com.google.firebase:firebase-bom:" + Versions.firebase_bom
+    }
+
+    object Module {
+
+    }
+
+    object Tool {
+        const val gradle: String = "com.android.tools.build:gradle:" + Versions.gradle
+
+        const val gradle_kotlin_plugin: String = "org.jetbrains.kotlin:kotlin-gradle-plugin:" + Versions.kotlin
+
+        /**
+         * https://github.com/pinterest/ktlint
+         */
+        const val ktlint: String = "com.pinterest:ktlint:" + Versions.ktlint
+
+        const val navigation_safe_args: String = "androidx.navigation:navigation-safe-args-gradle-plugin:" + Versions.navigation
+
+        const val google_services: String = "com.google.gms:google-services:" + Versions.google_service
+    }
+
+    object Test {
+        const val core_testing: String = "androidx.arch.core:core-testing:" + Versions.core_testing
+
+        /**
+         * https://developer.android.com/testing
+         */
+        const val espresso_contrib: String = "androidx.test.espresso:espresso-contrib:" +
+            Versions.espresso
+
+        const val espresso_core: String = "androidx.test.espresso:espresso-core:" +
+            Versions.espresso
+
+        const val espresso_intents: String = "androidx.test.espresso:espresso-intents:" +
+            Versions.espresso
+
+        const val fragment_testing: String =
+            "androidx.fragment:fragment-testing:" + Versions.fragment_testing
+
+        /**
+         * http://junit.org
+         */
+        const val junit: String = "junit:junit:" + Versions.junit
+
+        const val junit_android: String = "androidx.test.ext:junit:" + Versions.junit_android
+
+        const val koin: String = "org.koin:koin-test:" + Versions.koin
+
+        /**
+         * https://github.com/Kotlin/kotlinx.coroutines
+         */
+        const val kotlinx_coroutines_test: String =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-test:" +
+                Versions.coroutines
+
+        /**
+         * http://mockk.io
+         */
+        const val mockk: String = "io.mockk:mockk:" + Versions.mockk
+
+        const val mockk_android: String = "io.mockk:mockk-android:" + Versions.mockk
+
+        const val test_runner: String = "androidx.test:runner:" + Versions.runner
+
+        const val mockito_inline: String = "org.mockito:mockito-inline:" + Versions.mockito_inline
+    }
+
+    object UI {
+
+        const val card_view: String = "androidx.cardview:cardview:" + Versions.card_view
+
+        const val constraint_layout: String = "androidx.constraintlayout:constraintlayout:" +
+            Versions.constraint_layout
+
+        /**
+         * https://github.com/bumptech/glide
+         */
+        const val glide: String = "com.github.bumptech.glide:glide:" + Versions.glide
+
+        const val glide_annotations: String =
+            "com.github.bumptech.glide:annotations:" + Versions.glide
+
+        const val glide_compiler: String = "com.github.bumptech.glide:compiler:" + Versions.glide
+
+        const val material: String = "com.google.android.material:material:" + Versions.material
+
+        /**
+         * AndroidX Components and Tools
+         * https://developer.android.com/jetpack/androidx
+         */
+
+        const val recycler_view: String =
+            "androidx.recyclerview:recyclerview:" + Versions.recycler_view
+
+        /**
+         * http://facebook.github.io/shimmer-android
+         */
+        const val shimmer: String = "com.facebook.shimmer:shimmer:" + Versions.shimmer
+    }
+}
---
+++ buildSrc/src/main/java/com/lucasdias/buildSrc/Versions.kt
@@ -0,0 +1,50 @@
+package com.lucasdias.buildSrc
+
+/**
+ * Artigo com implementação parecida: https://proandroiddev.com/gradle-dependency-management-with-kotlin-94eed4df9a28
+ */
+object Versions {
+
+    /**
+     * External dependency versions
+     */
+    const val app_compat = "1.2.0"
+    const val card_view = "1.0.0"
+    const val constraint_layout = "2.0.4"
+    const val core_ktx = "1.2.0"
+    const val core_testing = "2.1.0"
+    const val coroutines = "1.4.1"
+    const val espresso = "3.2.0"
+    const val firebase_bom = "29.1.0"
+    const val fragment_testing = "1.2.4"
+    const val gradle = "4.1.0"
+    const val glide = "4.13.2"
+    const val google_service = "4.3.10"
+    const val junit = "4.12"
+    const val junit_android = "1.1.1"
+    const val koin = "3.2.0"
+    const val kotlin = "1.4.20"
+    const val ktlint = "0.43.1"
+    const val legacy_support = "1.0.0"
+    const val lifecycle_view_model = "2.2.0"
+    const val logging_interceptor = "4.2.1"
+    const val material = "1.2.1"
+    const val mockk = "1.9.3"
+    const val mockito_inline = "4.5.1"
+    const val navigation = "2.3.1"
+    const val preference = "1.1.1"
+    const val recycler_view = "1.1.0"
+    const val retrofit = "2.7.1"
+    const val runner = "1.2.0"
+    const val shimmer = "0.5.0"
+    const val timber = "3.1.0"
+
+    /**
+     * Build.Gradle configuration versions
+     */
+    const val compile_sdk = 30
+    const val build_tools = "30.0.0"
+    const val jvm_target_version = "1.8"
+    const val min_sdk = 28
+    const val target_sdk = 30
+}
