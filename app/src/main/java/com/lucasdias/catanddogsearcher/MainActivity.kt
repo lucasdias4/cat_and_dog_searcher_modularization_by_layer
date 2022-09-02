6abfcecacd3f789e4e1aa621e5680f5a517dd130
lucasdias4 <diasc.lucas@gmail.com>
2022-09-02 18:13:50 +0100

    Project created

---
+++ app/src/main/java/com/lucasdias/catanddogsearcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lucasdias.catanddogsearcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
