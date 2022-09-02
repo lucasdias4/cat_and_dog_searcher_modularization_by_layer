42f35f309f8429332bb92fd5c59a916385458906
lucasdias4 <diasc.lucas@gmail.com>
2022-09-02 21:28:52 +0100

    feature: search view module and search view model module dependencies setup

--- _presentation/search/view/build.gradle.kts
+++ _presentation/search/view/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.lucasdias.buildSrc.Dependency
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -33,11 +35,13 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(project(Dependency.Module.android_core))
+    implementation(project(Dependency.Module.resource))
+    implementation(project(Dependency.Module.search_view_model))
+    implementation(Dependency.Core.core_ktx)
+    implementation(Dependency.Core.appcompat)
+    implementation(Dependency.DI.koin)
+    implementation(Dependency.UI.constraint_layout)
+    implementation(Dependency.UI.material)
+    implementation(Dependency.UI.shimmer)
 }
